c4671111c16ec563758aaf5577b33cd37fe0a141
Shrujal00 <shrujal00@gmail.com>
2025-07-09 18:26:53 +0530

    bestBuy Water Trapped

---
+++ Arrays/trappingRainWater.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void TrapWater(int *height, int n) {
+    int leftMax[20000];
+    leftMax[0] = height[0];
+    int rightMax[20000];
+    rightMax[n-1] = height[n-1];
+    for(int i = 0; i < n; i++) {
+        leftMax[i] = max(leftMax[i-1],height[i-1]);
+    }
+    for (int i = n-2; i > 0; i--)
+    {
+        rightMax[i] = max(rightMax[i+1],height[i+1]);
+    }
+    int waterTrapped = 0;    
+    for (int i = 0; i < n; i++)
+    {
+        int currentWater = min(rightMax[i], leftMax[i]) - height[i];
+        if (currentWater > 0)
+        {
+            waterTrapped += currentWater;
+        }
+    }
+
+    cout << "Water trapped = " << waterTrapped << endl;
+}
+
+
+int main() {
+    int height[] = {4,2,0,6,3,2,5};
+    int n = sizeof(height)/sizeof(int);
+    TrapWater(height,n);
+    return 0;
+}
