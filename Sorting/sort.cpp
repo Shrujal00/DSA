772c2b6ea7f59dbecf718cbe47a91479785b6cc3
Shrujal00 <shrujal00@gmail.com>
2025-07-14 21:19:30 +0530

    Selection Sort

--- Sorting/sort.cpp
+++ Sorting/sort.cpp
@@ -24,13 +24,29 @@ void bubbleSort(int *arr, int n){
 }
 
 
-
+void selectionSort(int *arr, int n) {
+    for (int i = 0; i < n-1; i++)
+    {
+        int minidx = i;
+        for (int j = i+1; j < n; j++)
+        {
+            if (arr[j] < arr[minidx])
+            {
+                minidx = j;
+            }
+            swap(arr[i],arr[minidx]);
+        }
+    }
+}
 
 
 int main() {
     int arr[] = {5,4,1,3,2};
     int n = 5;
     bubbleSort(arr, n);
+    printArr(arr,n);
+    cout << endl;
+    selectionSort(arr,n);
     printArr(arr,n);
     return 0;
 }
