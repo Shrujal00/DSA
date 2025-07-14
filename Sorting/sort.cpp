c3e864a6a045f93f492bf7f236fe54bdb23f8668
Shrujal00 <shrujal00@gmail.com>
2025-07-14 22:07:52 +0530

    insertion Sort

--- Sorting/sort.cpp
+++ Sorting/sort.cpp
@@ -39,6 +39,20 @@ void selectionSort(int *arr, int n) {
     }
 }
 
+void insertionSort(int *arr, int n) {
+    for (int i = 1; i < n; i++)
+    {
+        int curr = arr[i];
+        int prev = i-1;
+        while (prev >= 0 && arr[prev] > curr)
+        {
+            swap(arr[prev],arr[prev+1]);
+            prev--;
+        }
+        arr[prev+1]  = curr;
+    }
+}
+
 
 int main() {
     int arr[] = {5,4,1,3,2};
