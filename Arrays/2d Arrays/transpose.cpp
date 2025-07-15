edb291fcaf401d6206c2b9e795116c644d26eb2f
Shrujal00 <shrujal00@gmail.com>
2025-07-15 19:21:27 +0530

    Transpose

--- Arrays/2d Arrays/transpose.cpp
+++ Arrays/2d Arrays/transpose.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
 
+
 int main() {
-    
+    int arr[2][3] = {{1,2,3},{5,6,7}};
+    int row = 2;
+    int column = 3;
+
+    int transpose[column][row];
+
+    for(int i = 0; i < row; i++) {
+        for(int j = 0; j < column; j++) {
+            transpose[j][i] = arr[i][j];
+        }
+    }
+    for(int i = 0; i < column; i++) {
+        for(int j = 0; j < row; j++) {
+            cout << transpose[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
 }
