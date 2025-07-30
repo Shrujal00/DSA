37a6b6a130b6fa463d97d71ac9ec8ca1a9e354c1
Shrujal <12402130601063@adit.ac.in>
2025-07-30 20:10:31 +0530

    tiling Problem

---
+++ Recursion/tilingProblem.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int tp(int n) { // 2xn
+    if(n == 0 || n == 1) {
+        return 1;
+    }
+    return (tp(n-1) + tp(n-2));
+}
+
+
+int main() {
+    int n = 4;
+    cout << tp(n) << endl;
+}
