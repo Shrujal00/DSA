5a350525881d3fdd9985e64cae5e937ba2f6690d
Shrujal <12402130601063@adit.ac.in>
2025-07-31 07:30:41 +0530

    remove Dumplicates from String Using recursion

--- Recursion/removeDuplicates.cpp
+++ Recursion/removeDuplicates.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    string str = "SSRRUJAL";
+void removeDuplicates(string str, string ans, int i, int map[26]){
+    if(i == str.size()) {
+        cout << "Ans: " << ans << endl;
+    }
+
+    int mapIdx = (int)(str[i] - 'a');
     
+    if(map[mapIdx]) { // duplicate
+        removeDuplicates(str,ans,i+1,map);
+    } else{ // not duplicate
+        map[mapIdx] = true;
+        removeDuplicates(str, ans+str[i], i+1, map);
+    }
+}
+
+
+int main() {
+    string str = "sshhrrujal";
+    string ans = "";
+    int map[26] = {false};
+    removeDuplicates(str,ans,0,map);
+    return 0;
 }
