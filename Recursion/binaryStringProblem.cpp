090595a9739fea8e8c2ec6496c8d0e1d63d8d0f0
Shrujal <12402130601063@adit.ac.in>
2025-08-02 09:17:07 +0530

    friends pairing binary String

---
+++ Recursion/binaryStringProblem.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+//Print Binary String of Size N without any consecutive 1s.
+void binaryString( int n, int lastplace, string ans) {
+    if(n == 0) {
+        cout << ans << endl;
+        return;
+    }
+    
+    if(lastplace != 1) {
+        binaryString(n-1, 0, ans+'0');
+        binaryString(n-1, 0, ans+'1');
+    } else {
+        binaryString(n-1,0,ans+'0');
+    }
+}
+
+int main() {
+    string ans = "";
+    binaryString(3, 0, ans);
+}
